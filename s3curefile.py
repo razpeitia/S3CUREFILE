d77532245c65310843adb2e6e68648556a10b4a2
Augusto Vera y Aguilar <averaguilar@gmail.com>
2017-04-15 21:13:12 -0500

    Added fixed verify
    It locates the changed hash file and signals in in the log.
    I enhanced the log writing also.
    So basically now it can create via command line a new SHA256 hash database of all the files under a path when given a path as cmd, and then it can verify all the files in the stored path against the XML database hashes to check if any file has changed.

--- s3curefile.py
+++ s3curefile.py
@@ -11,75 +11,97 @@ from xml.etree import ElementTree as elTree
 
 def mainGenerateHashes(pathToRead):
     countFiles = 0
-   
-    if not logExists():
-        initLog('s3curefilelog.csv')
-    writeToLog('hash database creation start path '+ pathToRead)
+    xmlDBFile = 's3curefiledb.xml'
+    logFileName = 's3curefiledb.csv'
+
+    if not logExists(logFileName):
+        initLog(logFileName)
+    writeToLog(logFileName, '>>> HASH DB CREATION STARTED: '+ pathToRead)
 
     #Read the folder structure and save in XML Database
-    root = elTree.Element('AllItems')     # create the element first for XML root
+    root = elTree.Element('AllItems')     # create the element Allitems for XML root
     tree = elTree.ElementTree(root)       # and pass it to the created tree
     BLOCKSIZE = 65536 # Block size to read in case of a large file.
-    hasher = hashlib.sha256()
+    
     for fileName in glob.iglob(pathToRead + '**\*.*', recursive=True): 
+        hasher = hashlib.sha256()
         with open(fileName, 'rb') as afile:
             buf = afile.read(BLOCKSIZE)
             while len(buf) > 0:  # as long as a block was read
                 hasher.update(buf)
                 buf = afile.read(BLOCKSIZE)
+        afile.close()        
         print(hasher.hexdigest())
         root.append(dict_to_elem({'filename':fileName, 'SHA256hash': hasher.hexdigest()}))
         countFiles += 1
-    # use last row of XML to store the total number of files hashed
-    root.append(dict_to_elem({'filename':'totalfiles', 'SHA256hash': str(countFiles)}))
+
+    # use last row of XML to store the base path and total number of files hashed
+    root.append(dict_to_elem({'basepath':pathToRead, 'totalfiles': str(countFiles)}))
     # Write the XML Database with all the filenames and hashes extracted
-    xmlDBFile = 's3curefiledb.xml'
     with open(xmlDBFile, 'w', encoding='utf-8') as file:
         tree.write(file, encoding='unicode')
-    writeToLog('total files hashed' + str(countFiles))
+    writeToLog(logFileName, 'total files hashed: ' + str(countFiles))
 
 def mainVerify():
-    writeToLog('hash verification start')
-    file_name = 's3curefiledb.xml'
+    logFileName = 's3curefiledb.csv'
+    xmlFileName = 's3curefiledb.xml'
+
     # Generate the tree of XML in memory
-    dom = elTree.parse(file_name)
-    # pulls all the FILEITEMs under the root along with all the subelements: filename y SHA256hash
+    dom = elTree.parse(xmlFileName)
+    # pulls all the FILEITEMs under the root along with all the subelements: filename and SHA256hash
     fileItems = dom.findall('FILEITEM')
     # extract the base path from the first filename found in XML db
-    fileNam = dom.findtext("FILEITEM/filename")
-    verifyPath = fileNam[:fileNam.index('\\', fileNam.index('\\') + 1)]
-    
+    # fileNam = dom.findtext("FILEITEM/filename")                             #<<<--- Extract the entire path and filename
+    # verifyPath = fileNam[:fileNam.index('\\', fileNam.index('\\') + 1)]
+    verifyPath = dom.findtext("FILEITEM/basepath")                             #<<<--- Extract the entire path and filename
+    writeToLog(logFileName, '>>> HASH VERIFICATION START ' + verifyPath)
     print(">>>> ", verifyPath)     
     # files = dom.findall('FILEITEM/filename') 
     # pulls only the subelements filename 
     # leaving the SHA25hash subelement
-    for fileItemsIndex in range(0,len(fileItems)): # traverse 1 by 1 all the FILEITEMs
+    BLOCKSIZE = 65536 # Block size to read in case of a large file.
+    # hasher = hashlib.sha256()
+    for fileItemsIndex in range(0,len(fileItems)-1):    # traverse 1 by 1 all the FILEITEMs until len -1 
+                                                        # because the last FILEITEM is the base path, and file counter.
         # then for every FILEITEM in turn, extract the filename and the paired SHA256hash
+        evalList = []
         for chld in fileItems[fileItemsIndex]: 
-            print(chld.text, end = "") # imprimo ambos segun salgan
-            print(" ")
-        print(" ")
+            evalList.append(chld.text)
         # print('File: ' + files[fil].text, ' Hash: ', hashes[fil].text)
-        print('verified')
+        print('evalList: ', evalList)
+        hasher = hashlib.sha256()
+        with open(evalList[0], 'rb') as afile: # open the file in the database FILEITEM/filename for binary read
+            buf = afile.read(BLOCKSIZE)
+            while len(buf) > 0:  # as long as a block was read
+                hasher.update(buf)
+                buf = afile.read(BLOCKSIZE)
+        afile.close()
+        hashFromFile = hasher.hexdigest()       # Generate the SHA256 file's value
+
+        if hashFromFile != evalList[1]:
+            logmsg = "* WARNING * " + evalList[0] + ' hash: ' + hashFromFile + ' db hash: ' + evalList[1]
+        else:
+            logmsg = 'file OK ' + evalList[0]
+        writeToLog(logFileName, logmsg)
 
 
-def logExists():
-    return os.path.isfile("s3curefilelog.csv")
+def logExists(logName):
+    return os.path.isfile(logName)
 
 
 def initLog(logName):
-    with open(logName, 'w') as csvfile:
-        fieldnames = ['datetime', 'message']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+    with open(logName, 'w') as logFile:
+        logFileWriter = csv.writer(logFile)
+        logFileWriter.writerow([time.strftime('%d/%m/%Y ') + time.strftime('%H:%M:%S'), '[*** log file created ***] > '])
+    logFile.close()
 
 
-def writeToLog(message):
-    with open('s3curefilelog.csv', 'a') as csvfile:
-        fieldnames = ['datetime', 'message']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writerow({'datetime': time.strftime('%d/%m/%Y|') + time.strftime('%H:%M:%S'), 'message': '[' + message + '] > '})
-
+def writeToLog(logName, message):
+    with open(logName, 'a') as logFile:
+        logFileWriter = csv.writer(logFile)
+        logFileWriter.writerow([time.strftime('%d/%m/%Y ') + time.strftime('%H:%M:%S'), '[' + message + '] > '])
+    logFile.close()
+    
 
 # Create one record for XML database of file hashes
 def dict_to_elem(dictionary):
@@ -92,21 +114,24 @@ def dict_to_elem(dictionary):
 
 
 if __name__ == "__main__":
+    # mainVerify()
+    # mainGenerateHashes('D:\\PASAR A USB\\')
+    logFile = 's3curefiledb.csv'
        # Check the command line arguments
     if len(sys.argv) == 2:
-        writeToLog('cmd line ' + sys.argv[1])
+        writeToLog(logFile, 'cmd line ' + sys.argv[1])
         if sys.argv[1] == '-v':  # <<<--- if the command passed was -v, then verify files in the path against the XML database
             mainVerify()
         else:
             dirArgument = sys.argv[1] + '\\' # it works if the path is ended in \ or not
             if not os.path.exists(dirArgument):
-                writeToLog('Path does not exist ' + sys.argv[1])
+                writeToLog(logFile, 'Path does not exist ' + sys.argv[1])
             elif not os.path.isdir(dirArgument):
-                writeToLog('Path is not a directory ' + sys.argv[1])
+                writeToLog(logFile, 'Path is not a directory ' + sys.argv[1])
             else:
                 print(dirArgument)
                 mainGenerateHashes(dirArgument) # <<<--- If the command passed was a path, generate the hashes
         exit(0)
     else:
-        writeToLog('incorrect cmd line USAGE: s3curefile (PATH or -v)')
+        writeToLog(logFile, 'incorrect cmd line USAGE: s3curefile (PATH or -v)')
         exit(1) #regresa estatus 1, checar con echo $?
